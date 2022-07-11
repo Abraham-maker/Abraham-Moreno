32e5f728ec16f2f6490cd7b98269c1e78b6f1642
Abraham-maker <imnoteverywhere12@gmail.com>
2022-07-11 11:17:16 -0400

    footer

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,17 +3,21 @@ import './Projects.css'
 
 const Projects = () => {
   return (
-    <div className='projects__container'>
-      <div className="projects__items">
-        asdsad
+    <div className="flex-container">
+      <div className="flex-child">
+        <p></p>
+        <a className='website'>Visit Website</a>
       </div>
-      <div className="projects__items">
-        asdsad
+      <div className="flex-child">
+        <p></p>
+        <a className='website'>Visit Website</a>
       </div>
-      <div className="projects__items">
-        asdsad
+      <div className="flex-child">
+        <p></p>
+        <a className='website'>Visit Website</a>
       </div>
     </div>
+
   )
 }
 
